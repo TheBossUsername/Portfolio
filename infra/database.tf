1392d8fed0eed2b2e85bf9a2053664ae3c470a51
BrendenScott <brenden.scott.it@outlook.com>
2026-08-26 16:35:36 -0600

    Fixed  database.tf: remove container item and fix partition key paths

--- infra/database.tf
+++ infra/database.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cosmosdb_account" "db" {
-  name = "cosmos-${var.project_prefix}-${var.environment}"
+  name                = "cosmos-${var.project_prefix}-${var.environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
@@ -14,7 +14,7 @@ resource "azurerm_cosmosdb_account" "db" {
   }
 
   geo_location {
-    location          = azurerm_resource_group.rg.location
+    location            = azurerm_resource_group.rg.location
     failover_priority = 0
   }
 }
@@ -30,18 +30,5 @@ resource "azurerm_cosmosdb_sql_container" "container" {
   resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.db.name
   database_name       = azurerm_cosmosdb_sql_database.sqldb.name
-  partition_key_path  = "/id"
-}
-
-resource "azurerm_cosmosdb_sql_container_item" "counter_item" {
-  container_name      = azurerm_cosmosdb_sql_container.container.name
-  database_name       = azurerm_cosmosdb_sql_database.sqldb.name
-  account_name        = azurerm_cosmosdb_account.db.name
-  resource_group_name = azurerm_resource_group.rg.name
-  partition_key       = "1"
-  
-  body = jsonencode({
-    "id"    = "1"
-    "count" = 0
-  })
+  partition_key_paths = ["/id"]
 }
